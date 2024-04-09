7cd2edd0c2519f40d142559541efc8b03ded260a
peerless2012 <peerless2012@126.com>
2024-04-09 11:22:23 +0800

    Add moc module for env variant.

--- app/src/main/java/com/peerless2012/abv/MainActivity.kt
+++ app/src/main/java/com/peerless2012/abv/MainActivity.kt
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import com.peerless2012.abv.impl.ImplPlugin
+import com.peerless2012.abv.mock.Mock
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val urlText = findViewById<TextView>(R.id.main_url)
+        urlText.text = "Url is ${Mock.Url}"
         val shareText = findViewById<TextView>(R.id.main_share)
         shareText.text = "Share is ${ShareCode.SHARED_CODE}"
         val implText = findViewById<TextView>(R.id.main_impl)
---
+++ lib_mock/src/main/java/com/peerless2012/abv/mock/Mock.kt
@@ -0,0 +1,14 @@
+package com.peerless2012.abv.mock
+
+/**
+ * @Author peerless2012
+ * @Email peerless2012@126.com
+ * @DateTime 2024/4/9 11:13
+ * @Version V1.0
+ * @Description
+ */
+object Mock {
+
+    const val Url = BuildConfig.URL
+
+}
